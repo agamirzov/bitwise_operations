5f547ca992c3fcbaf7812e5654cf150a970230c6
Evgeny Agamirzov <eagamirzov@gmail.com>
2017-02-02 15:29:06 +0100

    Added comments

--- sum_vs_xor/main.cpp
+++ sum_vs_xor/main.cpp
@@ -11,22 +11,32 @@
 using namespace std;
 
 /*
-Given an integer 'n' count how many 'x' satisfy the following conditions:
+Given an integer n count how many x satisfy the following conditions:
 0 <= x <= n
 n + x = n ^ x
 */
 
 int main() {
     long n;
-    int num_zeros = 0;
+    cout << "n = ";
     cin >> n;
+
+    /*
+    The expression n + x = n ^ x holds iff set bits of x and n are not overlapping i.e.
+    when xor operation does not flip bits of individual integers. Thus, all possible values of x
+    satisfying the initial condition fit in unset bits of n.
+    */
+
+    // Count the number of unset bits of n
+    int unset_bits = 0;
     while (n > 0) {
         if ((n & 1) == 0) {
-            ++num_zeros;
+            ++unset_bits;
         }
         n >>= 1;
     }
-    cout << "num_zeros = " << num_zeros << endl;
-    cout << "num_x = " << (1L << num_zeros) << endl;
+
+    // Within unset bits of n there are 2^unset_bits combinations i.e. values of x
+    cout << "num_x = " << (1L << unset_bits) << endl;
     return 0;
 }
